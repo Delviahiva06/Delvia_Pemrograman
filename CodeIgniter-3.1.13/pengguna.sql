64bbf225a0c570e43da56e8f7cef47ab5c45d78b
Delviahiva06 <delviahiva06@gmail.com>
2025-07-10 21:37:07 +0700

    Export ulang pengguna.sql: update data dan struktur terbaru

--- CodeIgniter-3.1.13/pengguna.sql
+++ CodeIgniter-3.1.13/pengguna.sql
@@ -1,16 +1,70 @@
--- Membuat tabel pengguna jika belum ada
-CREATE TABLE IF NOT EXISTS pengguna (
-    p_id INT AUTO_INCREMENT PRIMARY KEY,
-    nama VARCHAR(50) NOT NULL,
-    username VARCHAR(50) NOT NULL,
-    password VARCHAR(100) NOT NULL
-);
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 10, 2025 at 04:33 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
--- Hapus semua data lama
-delete from pengguna;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
--- Masukkan data baru
-INSERT INTO pengguna (nama, username, password) VALUES
-('Delvia', 'huruf hijaiyah', 'delvia123'),
-('Hiva', 'Huruf', 'hiva123'),
-('Karisma', 'Hijaiyah', 'karisma123'); 
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `tugas_delvia`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pengguna`
+--
+
+CREATE TABLE `pengguna` (
+  `p_id` int(11) UNSIGNED NOT NULL,
+  `nama` varchar(100) DEFAULT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `pengguna`
+--
+
+INSERT INTO `pengguna` (`p_id`, `nama`, `username`, `password`) VALUES
+(1, 'admin', 'admin', 'admin123'),
+(2, 'Delvia', 'delvia', 'delvia123'),
+(3, 'Hiva', 'hiva', 'hiva123'),
+(4, 'Karisma', 'karisma', 'karisma123');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `pengguna`
+--
+ALTER TABLE `pengguna`
+  ADD PRIMARY KEY (`p_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `pengguna`
+--
+ALTER TABLE `pengguna`
+  MODIFY `p_id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
